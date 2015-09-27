689ce83842ff8dea36255ddc4aac937798f78f76
Nicholas Pufal <nicholas@petrofeed.com>
2015-09-27 12:08:56 -0300

    display calculation form and results

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,26 @@
 angular.module('app.controllers', [])
 
-.controller('CalculatorCtrl', function($scope) {
+.controller('CalculatorCtrl', function($scope, $http, $interpolate, $ionicScrollDelegate) {
+  $scope.form = {
+    dependents: 0
+  };
+
+  $scope.clear = function clear () {
+    $scope.result = null;
+    $scope.form = {
+      dependents: 0
+    };
+    $ionicScrollDelegate.scrollTop();
+  };
+
+  $scope.calculate = function calculate () {
+    var endpoint = $interpolate('http://104.131.75.80:3000/calculate_net_salary?grossSalary={{grossSalary}}&dependents={{dependents}}');
+
+    $http.get(endpoint($scope.form)).then(function(result) {
+      $scope.result = result.data;
+    });
+  };
 })
 
 .controller('AboutCtrl', function($scope) {
-  console.log('bo');
 });
