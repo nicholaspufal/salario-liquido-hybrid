36ca7ff99a5bc77ab8e555db2bc8d271e2101217
Nicholas Pufal <nicholas@petrofeed.com>
2015-09-27 17:54:08 -0300

    first working version 👌

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,35 @@
 angular.module('app.controllers', [])
 
 .controller('CalculatorCtrl', function($scope, $http, $interpolate, $ionicScrollDelegate) {
-  $scope.form = {
-    dependents: 0
-  };
+  resetScope();
 
-  $scope.clear = function clear () {
-    $scope.result = null;
+  function resetScope () {
     $scope.form = {
       dependents: 0
     };
+    $scope.result = null;
+  }
+
+  function filterInput(input) {
+    var input = (input || 0).toString();
+    var input = input.replace(/,/, '.').replace(/[^0-9.]/g, '');
+    return input;
+  };
+
+  $scope.newSubmission = function newSubmission () {
+    resetScope();
     $ionicScrollDelegate.scrollTop();
   };
 
   $scope.calculate = function calculate () {
+    var requestData = {
+      grossSalary: filterInput($scope.form.grossSalary),
+      dependents: $scope.form.dependents
+    };
+
     var endpoint = $interpolate('http://104.131.75.80:3000/calculate_net_salary?grossSalary={{grossSalary}}&dependents={{dependents}}');
 
-    $http.get(endpoint($scope.form)).then(function(result) {
+    $http.get(endpoint(requestData)).then(function(result) {
       $scope.result = result.data;
     });
   };
